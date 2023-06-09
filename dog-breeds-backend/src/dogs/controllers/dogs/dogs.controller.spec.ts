e9c31c5df9c0f3054a9a508aaa6fb0490f2fa104
Leah <leahb_96@hotmail.co.uk>
2023-06-09 14:29:59 +0100

    module created + test file annotated to solidify my understanding

--- dog-breeds-backend/src/dogs/controllers/dogs/dogs.controller.spec.ts
+++ dog-breeds-backend/src/dogs/controllers/dogs/dogs.controller.spec.ts
@@ -2,13 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DogsController } from './dogs.controller';
 
 describe('DogsController', () => {
+  //declares controller variable with type 'DogsController'
   let controller: DogsController;
 
+  //before each 'hook' - calls this callback function before each test
   beforeEach(async () => {
+    // before each test, TestingModule is created -> creates a mock module
     const module: TestingModule = await Test.createTestingModule({
+      //dependencies
       controllers: [DogsController],
+      //complies everthing - once it is compiled, it retrieves the actual controller from the module by calling the get method and returns the instance of the dogs controller
     }).compile();
 
+    //.get takes a type or a token - this function is passed with the type + it will look for the correct instance and return the controller
     controller = module.get<DogsController>(DogsController);
   });
 
