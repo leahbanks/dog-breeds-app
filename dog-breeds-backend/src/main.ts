1f82aaaf9319f56e3e1a4a3d51b43bb7fffd8ab0
Leah <leahb_96@hotmail.co.uk>
2023-06-09 14:55:15 +0100

    enabled cors

--- dog-breeds-backend/src/main.ts
+++ dog-breeds-backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //allows front end to access back end on a different port
+  app.enableCors()
+
   await app.listen(3000);
 }
 bootstrap();
