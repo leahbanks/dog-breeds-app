9113013a6615f189f00c49349186c9d781bd6f0e
Leah <leahb_96@hotmail.co.uk>
2023-06-09 14:30:47 +0100

    created getDogs function to retrieve dogs service

--- dog-breeds-backend/src/dogs/controllers/dogs/dogs.controller.ts
+++ dog-breeds-backend/src/dogs/controllers/dogs/dogs.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DogsService } from 'src/dogs/services/dogs.service';
 
+//controller decorator accepts a prefix which is the base path to access the controller
+//everthing that is called with '/dogs' will hit this controller
 @Controller('dogs')
-export class DogsController {}
+export class DogsController {
+
+    //uses dependency injection to to retrieve the dogs service
+    constructor(private dogsService: DogsService) {}
+
+    //implement getDogs() method
+    //decorated with @Get to integrate with /dogs endpoint
+    @Get()
+    getDogs() {
+        return this.dogsService.getDogs();
+    }
+}
