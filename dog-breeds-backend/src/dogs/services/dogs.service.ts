b0f63fa85384aaa2e9e80701c73345f8a7f5a32b
Leah <leahb_96@hotmail.co.uk>
2023-06-15 09:55:17 +0100

    added extra comments

--- dog-breeds-backend/src/dogs/services/dogs.service.ts
+++ dog-breeds-backend/src/dogs/services/dogs.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { dogBreeds } from '../data/dogBreedData';
 
+// Injectable decorator marks the DogsService class as a provider, so it can be injected into the controller
 @Injectable()
 export class DogsService {
-  //retrieve dog data as a private field
+  //retrieve dog data as a private field (can't be accessed from outside the class)
   private dogs = dogBreeds;
 
-  //expose dog data over a getDogs() method that simply returns the players
+  //expose dog data over a getDogs() method that returns the dogs
   getDogs() {
     return this.dogs;
   }
